e18fffda64666dbaeab411f0c3c80a76032f5b88
dusk <exinyijiu@163.com>
2018-08-02 20:06:49 +0800

    view恢复

--- recon/views.py
+++ recon/views.py
@@ -18,14 +18,13 @@ tools = "/data/py/sec/dusksec/utils"
 def login(request):
     return render(request,"login.html")
 
-@accept_websocket
+
 def index(request):
-    if not request.is_websocket():
-        return render(request,'search.html')
-    else:
-        #ky = request.POST.get('keyword')
-        #ty = request.POST.get('seatype')
-        ty = request.websocket.read()
+    idtext = ""
+    ic = ""
+    if request.method == 'POST':
+        ky = request.POST.get('keyword')
+        ty = request.POST.get('seatype')
         if ty == "1":
             p = subprocess.Popen(tools+"/Sublist3r/sublist3r.py -d {}".format(ky),stdout=subprocess.PIPE,shell=True)
             idtext = p.stdout.read().strip()
